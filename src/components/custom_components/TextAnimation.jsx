467c0184ced4f5d2b94874c0a8f39021d2c4345a
Fiona Ni <FionaNi71@gmail.com>
2025-07-09 16:35:10 -0400

    Implemented Framer Motion

---
+++ src/components/custom_components/TextAnimation.jsx
@@ -0,0 +1,14 @@
+import { motion } from "motion/react"
+
+const TextAnimation = ({text}) => {
+    return (
+        <motion.h1 
+            initial={{ y: 25, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, ease: "easeInOut"}}>
+            <div>{text}</div>
+        </motion.h1>
+    )
+}
+
+export default TextAnimation;
